8a12480eda1f4a378da88f810d93ac63678f3260
Alena Malchikhina <alyonamalchikhina@gmail.com>
2017-07-07 22:51:04 +0300

    project structure

--- app/src/main/java/com/chog0/weatherappyandexschool/presentation/ui/MainActivity.java
+++ app/src/main/java/com/chog0/weatherappyandexschool/presentation/ui/MainActivity.java
@@ -1,4 +1,4 @@
-package com.chog0.weatherappyandexschool;
+package com.chog0.weatherappyandexschool.data;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.chog0.weatherappyandexschool.R;
 
 import butterknife.BindView;
 
